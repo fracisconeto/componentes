26323cd105c7f6da4a6b2e14a44acddc4baf017b
fracisconeto <vargasfrancisco227@gmail.com>
2024-07-12 15:08:10 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue';
+</script>
+
+<template>
+    <h1>Componentes</h1>
+    <hr>
+    <ExpandBox title="vargas lindo" content="tudo verdade" />
+    <ExpandBox title="vargas melhor do jifc" content="vai ir pro regional" />
+    <hr>
+</template>
+
+<style scoped></style>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, computed } from 'vue';
+const props = defineProps(['title', 'content'])
+
+const shoBox = ref(false)
+
+const TextoBotao = computed(() =>
+    shoBox.value ? 'Esconder' : ' Mostrar'
+);
+</script>
+
+<template>
+    <div>
+        <button @click="shoBox = !shoBox"> {{ TextoBotao }}</button>
+        <div v-if="shoBox" class="Expande-box">
+            <h1>{{ props.title }}</h1>
+            <h6>{{ props.content }}</h6>
+           <p> vargas o mais foda do if</p>
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.Expande-box {
+    height: 100%;
+    width: 400px;
+    padding: 20px;
+    border-radius: 10px;
+    border: 1px solid black;
+    box-shadow: 3px 3px 2px 2px gray;
+
+}
+
+
+
+
+
+;
+</style>
